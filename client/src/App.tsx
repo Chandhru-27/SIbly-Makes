1d7b33251ecfad50711b274763ba4fa1356aea17
Chandhru-27 <chandhru.l2007@gmail.com>
2025-12-31 18:25:35 +0530

    feat: implemented particles background

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,34 @@
 import Editorial from "./components/layout/Editorial";
 import SharedLogoAnimator from "./components/animation/SharedLogoAnimation";
-import HeaderContent from "./components/layout/HeaderContent";
 import HeroContent from "./components/layout/HeroContent";
 import Decorations from "./components/ui/Decorations";
 import { BrowserRouter } from "react-router-dom";
 import LenisProvider from "./hooks/LenisProvider";
 import Collections from "./components/layout/Collections";
 import Contact from "./components/layout/Contact";
+import Particles from "./components/Particles";
+import Footer from "./components/layout/Footer";
 
 const App = () => {
+  const viewPortHeight = window.innerHeight;
+
   return (
     <LenisProvider>
       <BrowserRouter>
         <main className="hero relative w-full min-h-screen max-w-640 mx-auto z-10">
+          <div style={{ width: "100%", height: "100%", position: "absolute" }}>
+            <Particles
+              particleColors={["#ffffff", "#7599C4", "#b30b86"]}
+              particleCount={viewPortHeight * 15}
+              particleSpread={10}
+              speed={0.05}
+              particleBaseSize={115}
+              moveParticlesOnHover={true}
+              alphaParticles={false}
+              sizeRandomness={1}
+              disableRotation={false}
+            />
+          </div>
           {/* Landing Page */}
           <div className="relative">
             <Decorations />
@@ -21,19 +37,27 @@ const App = () => {
             </div>
           </div>
 
-          {/* Editorial Page */}
-          <div className="mt-40 xl:mt-30">
+          {/* Editorial Section */}
+          <div className="mt-40">
             <Editorial />
             {SharedLogoAnimator()}
           </div>
 
-          <div className="mt-20 xl:mt-20">
-            <Collections/>
+          {/* Collection Section */}
+          <div className="mt-20">
+            <Collections />
           </div>
 
-          <div className="mt-20 xl:mt-60">
-            <Contact/>
+          {/* Contact Section */}
+          <div className="mt-20">
+            <Contact />
           </div>
+
+          {/* Footer section */}
+          <div className="mt-20">
+            <Footer/>
+          </div>
+
         </main>
       </BrowserRouter>
     </LenisProvider>
